c2c7ce17de10058a0e612144d78b12ac42a651d9
jeffdatura <stramoine7@gmail.com>
2021-09-07 17:35:16 +0200

    controller campus

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -25,11 +25,9 @@ class RegistrationController extends AbstractController
 
         if ($form->isSubmitted() && $form->isValid()) {
             // encode the plain password
-            $user->setPassword(
-                $passwordEncoder->encodePassword(
-                    $user,
-                    $form->get('plainPassword')->getData()
-                )
+            $user->setPassword($passwordEncoder->encodePassword(
+                $user,
+                $form->get('plainPassword')->getData())
             );
 
             $entityManager = $this->getDoctrine()->getManager();
