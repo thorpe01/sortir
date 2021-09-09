224db8f583baace74c9a32d66a7527b11b475486
jeffdatura <stramoine7@gmail.com>
2021-09-09 12:17:30 +0200

    go

---
+++ src/Controller/AfficherController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\ParticipantRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AfficherController extends AbstractController
+{
+    /**
+     * @Route("/afficher/{id}", name="afficher")
+     */
+    public function index(int $id, ParticipantRepository $participantRepository): Response
+    {
+        $participant = $participantRepository->find($id);
+        if (!$participant){
+            throw $this->createNotFoundException('not found');
+        }
+
+        return $this->render('afficher/index.html.twig', [
+            "participant" => $participant
+        ]);
+    }
+}
