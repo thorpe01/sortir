aaa8483d144b2418ebdfec078e17fd37c71340b9
thorpe01 <exercice0007@gmail.com>
2021-09-10 17:25:14 +0200

    css

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -2,19 +2,33 @@
 
 namespace App\Controller;
 
+
+use http\Env\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class AccueilController extends AbstractController
 {
     /**
+     * Class AccueilController
+     * @package AppController
      * @Route("/accueil", name="accueil")
      */
-    public function index(): Response
+    public function index(ParticipantRepository $participantRepository)
     {
+//        $repository = $this->getDoctrine()->getRepository(Participant::class);
+//        $participant = $repository->findOneBy(
+//            'nom');
+        $p1=$participantRepository->findOneBy([
+            'nom'
+        ]);
+
         return $this->render('accueil/index.html.twig', [
             'controller_name' => 'AccueilController',
+          'participant'=>$p1,
+
+
         ]);
     }
+
 }
--- src/Controller/CampusController.php
+++ src/Controller/CampusController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use http\Env\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -16,5 +17,13 @@ class CampusController extends AbstractController
         return $this->render('campus/index.html.twig', [
             'controller_name' => 'CampusController',
         ]);
+    }
+    public function delete(Request $request,Campus $nom){
+
+        $em= $this->getDoctrine()->getManager();
+
+        $em->remove($nom);
+        $em->flush();
+        return $this->redirectToRoute('profil');
     }
 }
